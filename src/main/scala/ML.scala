fd9077b6d373f6c41c51eea84ac88aa473ea09e2
disssid <siddharthsoni993@gmail.com>
2019-09-06 10:56:37 +0100

    Add datasets to .gitignore

--- src/main/scala/ML.scala
+++ src/main/scala/ML.scala
@@ -1,27 +1,27 @@
-/* SimpleApp.scala */
-
-import org.apache.spark
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
+case class Sales(date: String, date_block_num: Int, shop: Int, item_id: Int, item_price: Double, item_cnt_day: Double)
+
 object ML {
+
   def main(args: Array[String]) {
     val spark = SparkSession.builder
       .master("local[*]")
-      .appName("ML").getOrCreate()
-    import spark.implicits._
-    val df = trainingSet(spark)
-    print(s"Hello:  ${df.count()}")
+      .appName("ML")
+      .getOrCreate()
+    val df:Dataset[Sales] = trainingSet(spark)
+
+
+
     spark.stop()
   }
 
-  def count(df: DataFrame): Long = {
-    df.count()
-  }
-
-  def trainingSet(spark: SparkSession): DataFrame = {
+  def trainingSet(spark: SparkSession): Dataset[Sales] = {
+    import spark.implicits._
     return spark.read.format("csv")
       .option("header", "true")
       .option("inferSchema", "true")
       .load("sales_train.csv")
+      .as[Sales]
   }
 }
